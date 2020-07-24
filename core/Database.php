f12299c7d2cc3107a3b2dc7271abfff3af6fca04
Renato Novaes <renatoalmeida49@gmail.com>
2020-07-24 11:33:00 -0300

    Correção no arquivo de bando de dados

--- core/Database.php
+++ core/Database.php
@@ -8,13 +8,13 @@ class Database {
 
             try {
                 $pdo = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
-
-                return $pdo;
             } catch (PDOException $e) {
                 echo "ERRO: ".$e->getMessage();
                 exit;
             }
         }
+
+        return $pdo;
     }
 
     private function __construct(){}
